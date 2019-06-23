35e8378cbafa80aff4cc50676b713cb75e261a92
dowait <dabing@163.com>
2019-06-23 13:16:56 +0800

    classpath

--- src/main/java/com/jvm/learn/classLoad/Learn13.java
+++ src/main/java/com/jvm/learn/classLoad/Learn13.java
@@ -3,6 +3,9 @@ package com.jvm.learn.classLoad;
 /**
  *
  * 查询jvm属性路径
+ *
+ *      在target的classes路径下使用命令 java com.jvm.learn.classLoad.Learn13 运行时, classpath
+ * 的输出结果为.点, 表示当前目录.
  * @Author dabing
  * @Date 2019-06-22 17:29
  **/
